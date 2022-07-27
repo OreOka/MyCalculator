22ef54b6f20897ef4e1d1a68cbe5034d140e55c7
Oreva Okaruefe <orevaokaruefe@gmail.com>
2022-07-26 20:52:21 -0500

    Done Calculator

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -6,10 +6,13 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import java.lang.ArithmeticException
 
 class MainActivity : AppCompatActivity() {
 
     private var tvInput:TextView? = null
+    var lastNumeric:Boolean = false
+    var lastDot :Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,10 +20,116 @@ class MainActivity : AppCompatActivity() {
         tvInput= findViewById(R.id.tvInput)
     }
 
+    fun removeZeroAfterDot(result: String): String {
+        var value = result
+        if (result.contains(".0"))
+            value = result.substring(0, result.length -2)
+
+        return value
+    }
     fun onDigit(view: View) {
         tvInput?.append((view as Button).text)
+        lastNumeric = true
+        lastDot = false
+
+        var sentence = "Oreva is disciplined"
+        if (sentence.contains("Oreva")) {
+
+        }
     }
+
+    fun onOperator(view: View) {
+        tvInput?.text?.let {
+            if (lastNumeric && !isOperatorAdded(it.toString())) {
+                tvInput?.append((view as Button).text)
+                lastNumeric = false
+                lastDot = false
+            }
+        }
+
+    }
+
+    private fun isOperatorAdded(value: String):Boolean {
+        return if (value.startsWith("-")) {
+            false
+        }else{
+            value.contains("/")
+                    ||value.contains("*")
+                    ||value.contains("+")
+                    ||value.contains("-")
+        }
+
+    }
+
+
     fun onClear(view: View) {
         tvInput?.text = ""
+    }
+    fun onDecimalPoint(view: View) {
+        if (lastNumeric && !lastDot) {
+            tvInput?.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
+    }
+
+    fun onEqual(view: View) {
+        if (lastNumeric) {
+            var tvValue  = tvInput?.text.toString()
+            var prefix = ""
+            try {
+                if (tvValue.startsWith("-")) {
+                    prefix = "-"
+                    tvValue = tvValue.substring(1)
+                }
+                if (tvValue.contains("-")) {
+                    val splitValue = tvValue.split("-")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if (prefix.isNotEmpty()) {
+                        one = prefix + one
+                    }
+                    tvInput?.text = removeZeroAfterDot((one.toDouble() - two.toDouble()).toString())
+                }
+
+                else  if (tvValue.contains("+")) {
+                    val splitValue = tvValue.split("+")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if (prefix.isNotEmpty()) {
+                        one = prefix + one
+                    }
+                    tvInput?.text = removeZeroAfterDot((one.toDouble() + two.toDouble()).toString())
+                }
+
+                else  if (tvValue.contains("/")) {
+                    val splitValue = tvValue.split("/")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if (prefix.isNotEmpty()) {
+                        one = prefix + one
+                    }
+                    tvInput?.text = removeZeroAfterDot((one.toDouble() / two.toDouble()).toString())
+                }
+
+                else  if (tvValue.contains("*")) {
+                    val splitValue = tvValue.split("*")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if (prefix.isNotEmpty()) {
+                        one = prefix + one
+                    }
+                    tvInput?.text = removeZeroAfterDot((one.toDouble() + two.toDouble()).toString())
+                }
+
+
+            } catch (e: ArithmeticException) {
+              e.printStackTrace()
+            }
+        }
     }
 }
